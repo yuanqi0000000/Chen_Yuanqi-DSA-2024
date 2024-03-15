beca9dc5cc27f1d427fb2ea98f6572ceb750b333
yuanqi0000000 <3331039137@qq.com>
2024-03-15 10:18:21 +0800

    call generic methods in Algorithms class

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -11,28 +11,16 @@ public class Grades {
          this.grades[counter] = grades[counter];
       }
    }
+ 
 
    public void reverse() {
-      
-      int i = 0;
-      while (i <= (grades.length-1)/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      Algorithms.reverse(grades);
    }
 
    public void sort() {
-
-      for(int i=1;i < grades.length;i++){
-         for(int j=i; (j>0) && (grades[j] < grades[j-1]);j--){
-            int tmp = grades[j];
-            grades[j] = grades[j-1];
-            grades[j-1] = tmp;
-         }
-      }
+      Algorithms.sort(grades);
    }
+
 
 
    public Integer [] getArray() {
