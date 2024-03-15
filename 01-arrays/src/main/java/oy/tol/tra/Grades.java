8f25faeae9349f955c1f1dd7bc505afaf8d86085
yuanqi0000000 <3331039137@qq.com>
2024-03-15 09:55:04 +0800

    fix Grades.java and implement sorting and reversing as generic algorithms

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -32,7 +32,7 @@ public class Grades {
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
       int i = 0;
-      while (i <= grades.length/2) {
+      while (i <= (grades.length-1)/2) {
          int temp = grades[i];
          grades[i] = grades[grades.length-i-1];
          grades[grades.length-i-1] = temp;
@@ -52,14 +52,21 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int i = grades.length-1;
-      while (i > 0) {
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
+      // int i = grades.length-1;
+      // while (i > 0) {
+      //    if (grades[i] < grades[i-1]) {
+      //       int tmp = grades[i];
+      //       grades[i] = grades[i-1];
+      //       grades[i-1] = tmp;
+      //    }
+      //    i--;
+      // }
+      for(int i=1;i < grades.length;i++){
+         for(int j=i; (j>0) && (grades[j] < grades[j-1]);j--){
+            int tmp = grades[j];
+            grades[j] = grades[j-1];
+            grades[j-1] = tmp;
          }
-         i--;
       }
    }
 
