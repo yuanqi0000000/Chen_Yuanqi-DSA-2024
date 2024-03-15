d511a1d91ab507b93192c695c8400f65ada26ba1
yuanqi0000000 <3331039137@qq.com>
2024-03-15 10:00:06 +0800

    fix Grades.java and implement sorting and reversing as generic algorithms, remove comments

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,17 +1,10 @@
 package oy.tol.tra;
 
-/**
- * A simple array of student grades to be used in testing
- * misbehaving algorithm for reversing the array.
- */
+
 public class Grades {
    
    private Integer [] grades = null;
 
-   /**
-    * A constructor for building IntArrays.
-    * @param grades the plain Java integer array with numbers to add.
-    */
    public Grades(Integer [] grades) {
       this.grades = new Integer [grades.length];
       for (int counter = 0; counter < grades.length; counter++) {
@@ -19,18 +12,8 @@ public class Grades {
       }
    }
 
-   /**
-    * The method to reverse the internal Java int array.
-    */
    public void reverse() {
-      /* TODO:
-       1. Edit the test data files to see if the reverse() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
-      */
+      
       int i = 0;
       while (i <= (grades.length-1)/2) {
          int temp = grades[i];
@@ -40,27 +23,8 @@ public class Grades {
      }
    }
 
-   /**
-    * Sorts the array to ascending order.
-    */
    public void sort() {
-      /* TODO:
-       1. Edit the test data files to see if the sort() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
-      */
-      // int i = grades.length-1;
-      // while (i > 0) {
-      //    if (grades[i] < grades[i-1]) {
-      //       int tmp = grades[i];
-      //       grades[i] = grades[i-1];
-      //       grades[i-1] = tmp;
-      //    }
-      //    i--;
-      // }
+
       for(int i=1;i < grades.length;i++){
          for(int j=i; (j>0) && (grades[j] < grades[j-1]);j--){
             int tmp = grades[j];
@@ -70,10 +34,7 @@ public class Grades {
       }
    }
 
-   /**
-    * Returns the plain Java int [] array for investigation.
-    * @return The int array.
-    */
+
    public Integer [] getArray() {
       return grades;
    }
